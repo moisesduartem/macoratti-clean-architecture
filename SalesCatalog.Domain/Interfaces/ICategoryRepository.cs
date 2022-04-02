7b439d29ef5a18c4539666548e46178be1312de9
Moisés Mariano <moisesduartem@gmail.com>
2022-04-02 19:48:42 -0300

    chore: create repositories interfaces

---
+++ SalesCatalog.Domain/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using SalesCatalog.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SalesCatalog.Domain.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<Category>> GetAsync();
+        Task<Category> GetByIdAsync(int? id);
+        Task<Category> CreateAsync(Category category);
+        Task<Category> UpdateAsync(Category category);
+        Task<Category> RemoveAsync(Category category);
+    }
+}
---
+++ SalesCatalog.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using SalesCatalog.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SalesCatalog.Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetAsync();
+        Task<Product> GetByIdAsync(int? id);
+        Task<Product> GetByIdWithCategoryAsync(int? id);
+        Task<Product> CreateAsync(Product category);
+        Task<Product> UpdateAsync(Product category);
+        Task<Product> RemoveAsync(Product category);
+    }
+}
